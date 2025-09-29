14a01f51ce79786e096268b43fa6635bee32fa8a
VARADAkn <163798839+VARADAkn@users.noreply.github.com>
2025-09-29 19:11:47 +0530

    Update Workspace.js

--- src/models/Workspace.js
+++ src/models/Workspace.js
@@ -4,11 +4,13 @@ const sequelize = require("../config/db");
 const WorkSpace = sequelize.define(
   "WorkSpace",
   {
-    id: { type: DataTypes.INTEGER,autoIncrement:true, primaryKey: true },
-    companyId: { type: DataTypes.STRING },
-    workSpaceId: { type: DataTypes.STRING, allowNull: false },
-    doctorId: { type: DataTypes.STRING, allowNull: false },
-    createdBy: { type: DataTypes.STRING, allowNull: false },
+    id: { type: DataTypes.INTEGER, autoIncrement: true, primaryKey: true },
+    name: { type: DataTypes.STRING, allowNull: false },
+    description: { type: DataTypes.STRING },
+    address: { type: DataTypes.STRING },
+    phoneNumber: { type: DataTypes.STRING },
+    email: { type: DataTypes.STRING },
+    createdBy: { type: DataTypes.STRING },
   },
   {
     tableName: "workspaces",
