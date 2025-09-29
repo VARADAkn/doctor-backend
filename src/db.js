dbb63e8a20dd58218e4b32482dbbe99f0d541659
VARADAkn <163798839+VARADAkn@users.noreply.github.com>
2025-09-29 18:47:59 +0530

    Add files via upload

---
+++ src/db.js
@@ -0,0 +1,10 @@
+const { Sequelize } = require("sequelize");
+
+const sequelize = new Sequelize("CRUD", "postgres", "Varada@2003", {
+  host: "localhost",
+  dialect: "postgres",
+  port: 5432,
+  logging: false, // set true if you want to see raw SQL logs
+});
+
+module.exports = sequelize;
