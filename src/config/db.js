45fbd4e3288e518dfecb7fbf34d13e34f2f8e7e5
VARADAkn <163798839+VARADAkn@users.noreply.github.com>
2025-10-06 22:25:02 +0530

    Update db.js

--- src/config/db.js
+++ src/config/db.js
@@ -1,10 +1,19 @@
 const { Sequelize } = require("sequelize");
 
-const sequelize = new Sequelize("CRUD", "postgres", "Varada@2003", {
-  host: "localhost",
-  dialect: "postgres",
-  port: 5432,
-  logging: false, // set true if you want to see raw SQL logs
-});
+// This line loads the variables from your .env file into process.env
+require('dotenv').config();
+
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASS,
+  {
+    host: process.env.DB_HOST,
+    dialect: "postgres",
+    port: process.env.DB_PORT,
+    logging: false, // set true if you want to see raw SQL logs
+  }
+);
 
 module.exports = sequelize;
+
