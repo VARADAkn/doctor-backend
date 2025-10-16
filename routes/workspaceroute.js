3f950d16c8c301c6c0b37a620283490aaf59337f
VARADAkn <varada_22b090cs@gecwyd.ac.in>
2025-10-17 00:11:42 +0530

    initail commit

--- routes/workspaceroute.js
+++ routes/workspaceroute.js
@@ -3,12 +3,10 @@ const router = express.Router();
 const workspaceController = require('../controllers/workspacecontroller');
 
 // Defines routes for /workspaces
-
 router.post('/create', workspaceController.createWorkSpace);
 router.post('/update', workspaceController.updateWorkSpace);
 router.post('/get', workspaceController.getWorkSpaces);
 router.post('/find', workspaceController.getWorkSpace);
 router.post('/delete', workspaceController.deleteWorkSpace);
-
 
 module.exports = router;
